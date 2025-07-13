bb43cace79861e1d3e20257b152d490e6c308ec3
Abhi95081 <abhirou7@gmail.com>
2025-07-13 12:22:05 +0530

    contact Database Commit

---
+++ composeApp/src/commonMain/kotlin/org/example/project/database/Contact.kt
@@ -0,0 +1,12 @@
+package org.example.project.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact_table")
+data class Contact(
+    @PrimaryKey(autoGenerate = true) val id : Int? = null,
+    val name : String,
+    val number : String,
+    val email : String,
+)
